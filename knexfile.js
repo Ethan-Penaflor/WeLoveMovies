b04992aad1d29de03470860cf05f5dd7fe305734
Ethan Penaflor <ethanpenaflor20@gmail.com>
2024-07-29 20:55:50 -0500

    fixing knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,47 @@
-// Update with your config settings.
+const path = require("path");
+
+require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgresql://welovemovies_9e00_user:h3HPk0opu3HQy7TVLk7AqS0IYy1Ndr4s@dpg-cqca1tt6l47c73d09u3g-a/welovemovies_9e00",
+} = process.env;
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: path.join(dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(dirname, "src", "db", "seeds"),
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(dirname, "src", "db", "migrations"),
     },
-    pool: {
-      min: 2,
-      max: 10
+    seeds: {
+      directory: path.join(dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: path.join(dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
 };
