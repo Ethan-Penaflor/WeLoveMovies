3690c61613ba817c1250f6660f8c27cebaa3b9c7
Ethan Penaflor <ethanpenaflor20@gmail.com>
2024-07-29 20:15:22 -0500

    adding migrations

--- knexfile.js
+++ knexfile.js
@@ -3,37 +3,48 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DATABASE_URL // This is optional, used for local development
 } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      ssl: {
+        rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
+      }
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-    ssl: {
-      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
     }
   },
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+      connectionString: DATABASE_URL, // Use DATABASE_URL for production environment
+      ssl: {
+        rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
+      }
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-    ssl: {
-      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
     }
   },
 
