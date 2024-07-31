9d85feb02db5f69f863ad76a6371923ba4a6209a
Ethan Penaflor <ethanpenaflor20@gmail.com>
2024-07-30 20:48:46 -0500

    fixing components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const app = express();
 
 const moviesRouter = require("./movies/movies.router");
@@ -13,23 +12,15 @@ const notFound = require("./errors/notFound");
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../frontend/build"))); // Adjust the path
-
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter);
-
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html")); // Adjust the path
-});
 
 app.use(notFound);
 
 app.use((error, req, res, next) => {
     const { status = 500, message = "error" } = error;
-    res.status(status).json({ error: message })
+    res.status(status).json({ error: message });
 });
 
 module.exports = app;
