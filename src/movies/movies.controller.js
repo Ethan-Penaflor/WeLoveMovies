82a169899aa59aa3fb3c8169b7b050b32f0c8871
Ethan Penaflor <ethanpenaflor20@gmail.com>
2024-07-31 22:06:29 -0500

    fixing backend

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,23 +1,31 @@
 const moviesService = require("./movies.service");
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+
 
 async function list(req, res, next) {
     const data = await moviesService.list(req.query.is_showing);
     res.json({ data });
 }
 
-async function read(req, res, next) {
-    const { movie: data } = res.locals;
-    res.json({ data });
+async function read(req, res, next) { 
+    const { movie: data } = res.locals
+    res.json({ data })
 }
 
-async function movieExists(req, res, next) {
-    const movie = await moviesService.read(req.params.movieId);
-    if (movie) {
-        res.locals.movie = movie;
-        return next();
-    }
-    return next({ status: 404, message: `Movie cannot be found.` });
+
+function movieExists(req, res, next) {
+    moviesService.read(req.params.movieId)
+    .then((movie) => {
+        if (movie) {
+            res.locals.movie = movie;
+            return next();
+        }
+        next({ status: 404, message: `Movie cannot be found.` });
+        // res.status(404).json({
+        //     "error": "Movie cannot be found."
+        //   })
+    })
+    // .catch(console.log("THIS IS AN ERROR"));
 }
 
 async function listTheatersByMovie(req, res, next) {
@@ -32,9 +40,13 @@ async function listReviewsByMovie(req, res, next) {
     res.json({ data });
 }
 
+// console.log(JSON.stringify(data, null, 2))
+
 module.exports = {
     list: asyncErrorBoundary(list),
-    listTheatersByMovie: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listTheatersByMovie)],
-    listReviewsByMovie: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listReviewsByMovie)],
-    read: [asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
-};
+    listTheatersByMovie: [movieExists, asyncErrorBoundary(listTheatersByMovie)],
+    listReviewsByMovie: [movieExists, asyncErrorBoundary(listReviewsByMovie)],
+    // read: asyncErrorBoundary(read),
+    read: [movieExists, asyncErrorBoundary(read)],
+    
+}
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,6 +1,44 @@
 const knex = require("../db/connection");
 const mapProperties = require("../utils/map-properties");
 
+function listIfShowing() {
+    return knex("movies as m")
+    .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+    .select("m.*")
+    .groupBy("m.movie_id")
+    .where({ is_showing : true })
+}
+
+function list(is_showing) {
+    if (is_showing === "true") {
+        return listIfShowing();
+    }
+
+    return knex("movies").select("*");
+}
+
+function read(id) {
+    return knex("movies").select("*").where({ movie_id: id }).first();
+}
+
+function listTheatersByMovie(movieId) {
+    return knex("movies_theaters as mt")
+    .join("theaters", "mt.theater_id", "theaters.theater_id")
+    // .join("movies as m", "mt.movie_id", "m.movie_id")
+    // .distinct("mt.movie_id")
+    .select("*")
+    .where({ "movie_id": movieId })
+    .distinct()
+}
+
+function listReviewsByMovie(movieId) {
+    return knex("reviews")
+    .join("critics", "reviews.critic_id", "critics.critic_id")
+    .select("*")
+    .where({ "movie_id": movieId })
+    .then((data) => data.map(addCriticInfo))
+}
+
 const addCriticInfo = mapProperties({
     critic_id: "critic.critic_id",
     preferred_name: "critic.preferred_name",
@@ -10,45 +48,10 @@ const addCriticInfo = mapProperties({
     updated_at: "critic.updated_at",
 });
 
-function listIfShowing() {
-    return knex("movies as m")
-        .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
-        .select("m.*")
-        .groupBy("m.movie_id")
-        .where({ "mt.is_showing": true });
-}
-
-function list(is_showing) {
-    if (is_showing === "true") {
-        return listIfShowing();
-    }
-    return knex("movies").select("*");
-}
-
-function read(movieId) {
-    return knex("movies").select("*").where({ movie_id: movieId }).first();
-}
-
-function listTheatersByMovie(movieId) {
-    return knex("movies_theaters as mt")
-        .join("theaters as t", "mt.theater_id", "t.theater_id")
-        .select("t.*")
-        .where({ "mt.movie_id": movieId })
-        .distinct();
-}
-
-function listReviewsByMovie(movieId) {
-    return knex("reviews as r")
-        .join("critics as c", "r.critic_id", "c.critic_id")
-        .select("r.*", "c.*")
-        .where({ "r.movie_id": movieId })
-        .then((reviews) => reviews.map(addCriticInfo));
-}
-
 module.exports = {
     list,
     listIfShowing,
     listTheatersByMovie,
     listReviewsByMovie,
     read,
-};
+}
